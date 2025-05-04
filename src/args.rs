cf73d2337581a7053d3bfa0a1d208156fd8430c8
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2025-05-04 23:45:41 +0800

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -286,38 +286,38 @@ pub fn print_help() {
     
     // 基本参数
     println!("\n{}:", "基本参数".bold());
-    print_arg!("-url", "TLS 模式的 Httping 或下载测速所使用的测速地址 (https://example.com/file)", "[默认: 未指定]");
-    print_arg!("-urlist", "从 URL 内读取测速地址列表 (https://example.com/url_list.txt)", "[默认: 未指定]");
-    print_arg!("-f", "从文件或文件路径读取 IP 或 CIDR", "[默认: 未指定]");
-    print_arg!("-ip", "直接指定 IP 或 CIDR (多个用逗号分隔)", "[默认: 未指定]");
-    print_arg!("-ipurl", "从URL读取 IP 或 CIDR (https://example.com/ip_list.txt)", "[默认: 未指定]");
+    print_arg!("-url", "TLS 模式的 Httping 或下载测速所使用的测速地址（https://example.com/file）", "[默认: 未指定]");
+    print_arg!("-urlist", "从 URL 内读取测速地址列表（https://example.com/url_list.txt）", "[默认: 未指定]");
+    print_arg!("-f", "从文件或文件路径读取 IP 或 CIDR ", "[默认: 未指定]");
+    print_arg!("-ip", "直接指定 IP 或 CIDR（多个用逗号分隔）", "[默认: 未指定]");
+    print_arg!("-ipurl", "从URL读取 IP 或 CIDR （https://example.com/ip_list.txt) ", "[默认: 未指定]");
     print_arg!("-timeout", "程序超时退出时间（示例：1h3m6s）", "[默认: 不限制]");
     
     // 测速参数
     println!("\n{}:", "测速参数".bold());
-    print_arg!("-t", "延迟测速次数", "[默认: 4]");
-    print_arg!("-dn", "所需下载测速结果数量", "[默认: 10]");
+    print_arg!("-t", "延迟测速次数 ", "[默认: 4]");
+    print_arg!("-dn", "所需下载测速结果数量 ", "[默认: 10]");
     print_arg!("-dt", "下载测速时间（秒）", "[默认: 10]");
-    print_arg!("-tp", "测速端口", "[默认: 443]");
+    print_arg!("-tp", "测速端口 ", "[默认: 443]");
     print_arg!("-all4", "测速全部 IPv4 ", "[默认: 否]");
-    print_arg!("-tn", "当 Ping 到指定可用数量，提前结束 Ping", "[默认: 否]");
+    print_arg!("-tn", "当 Ping 到指定可用数量，提前结束 Ping ", "[默认: 否]");
     
     // 测速选项
     println!("\n{}:", "测速选项".bold());
-    print_arg!("-httping", "使用非 TLS 模式的 Httping ，无需测速地址", "[默认: 否]");
-    print_arg!("-ping", "ICMP-Ping 测速模式", "[默认: 否]");
-    print_arg!("-dd", "禁用下载测速", "[默认: 否]");
-    print_arg!("-hu", "使用 TLS 模式的 Httping ，可指定其 URL 测速地址，或作为无参数命令，使用其他测速地址", "[默认: 否]");
+    print_arg!("-httping", "使用非 TLS 模式的 Httping ，无需测速地址 ", "[默认: 否]");
+    print_arg!("-ping", "ICMP-Ping 测速模式 ", "[默认: 否]");
+    print_arg!("-dd", "禁用下载测速 ", "[默认: 否]");
+    print_arg!("-hu", "使用 TLS 模式的 Httping ，可指定其 URL 测速地址，或作为无参数命令，使用其他测速地址 ", "[默认: 否]");
     print_arg!("-colo", "匹配指定地区（示例：HKG,SJC）", "[默认: 未指定]");
-    print_arg!("-n", "动态线程池的线程数量上限", "[默认: 1024]");
+    print_arg!("-n", "动态线程池的线程数量上限 ", "[默认: 1024]");
     
     // 结果参数
     println!("\n{}:", "结果参数".bold());
     print_arg!("-tl", "延迟上限（毫秒）", "[默认: 2000]");
     print_arg!("-tll", "延迟下限（毫秒）", "[默认: 0]");
-    print_arg!("-tlr", "丢包率上限", "[默认: 1.00]");
+    print_arg!("-tlr", "丢包率上限 ", "[默认: 1.00]");
     print_arg!("-sl", "下载速度下限（MB/s）", "[默认: 0.00]");
-    print_arg!("-p", "终端显示结果数量", "[默认: 10]");
+    print_arg!("-p", "终端显示结果数量 ", "[默认: 10]");
     print_arg!("-o", "输出结果文件（文件名或文件路径）", "[默认: result.csv]");
 }
 
