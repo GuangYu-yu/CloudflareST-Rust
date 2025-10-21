878d9166cb66fb93309bb1fbe59b59fab720bc3c
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2025-10-21 23:17:33 +0800

    Update zigcc-wrapper.bat

--- scripts/zigcc-wrapper.bat
+++ scripts/zigcc-wrapper.bat
@@ -1,14 +1,15 @@
 @echo off
-setlocal enabledelayedexpansion
-
+setlocal DisableDelayedExpansion
 set "args="
-for %%A in (%*) do (
-    set "arg=%%A"
-    if /I "!arg!"=="/NOLOGO" (
-    ) else if /I "!arg:~0,11!"=="/PDBALTPATH:" (
-    ) else if /I "!arg:~0,8!"=="/NATVIS:" (
-    ) else (
-        set "args=!args! %%A"
-    )
-)
-zig cc !args!
+for %%A in (%*) do call :filter "%%~A"
+zig cc %args%
+exit /b %ERRORLEVEL%
+
+:filter
+setlocal EnableDelayedExpansion
+set "arg=%~1"
+if /I "%arg%"=="/NOLOGO" exit /b
+if /I "%arg:~0,11%"=="/PDBALTPATH:" exit /b
+if /I "%arg:~0,8%"=="/NATVIS:" exit /b
+endlocal & set "args=%args% %~1"
+exit /b
